ad7a2bca56d1c7328ae97149c616215a4c61083b
Daniel Gorodezki <daniel_gorodezki@mail.ru>
2023-05-20 10:58:35 +0600

    Add keyboard menu

--- bot.py
+++ bot.py
@@ -4,5 +4,18 @@ from telebot import types
 
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=["start"])
+def start(message):
+     markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+     item1=types.KeyboardButton("text1")
+     item2=types.KeyboardButton("text2")
+     item3=types.KeyboardButton("text3")
+     item4=types.KeyboardButton("text4")
+     markup.add(item1)	
+     markup.add(item2)
+     markup.add(item3)
+     markup.add(item4)
+     bot.send_message(message.chat.id, "Start message", reply_markup=markup)	
+
 if __name__ == '__main__':
      bot.infinity_polling()
